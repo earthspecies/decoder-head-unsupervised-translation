b6539657c8b704c856cdfb7b34798c4d948a21d4
Radek Osmulski <rosmulski@gmail.com>
2020-02-07 12:26:53 +0100

    update

--- decoder_head/_nbdev.py
+++ decoder_head/_nbdev.py
@@ -5,9 +5,9 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"split_file_into_chunks": "01_train_LM_en.ipynb",
          "PermuteEmbedding": "01_train_LM_en.ipynb",
          "pAWD_LSTM": "01_train_LM_en.ipynb",
-         "opt": "01_train_LM_en.ipynb",
-         "drops": "01_train_LM_en.ipynb",
-         "config": "01_train_LM_en.ipynb",
+         "opt": "04a_LM_with_normalized_embeddings_mixer_softmax.ipynb",
+         "drops": "04a_LM_with_normalized_embeddings_mixer_softmax.ipynb",
+         "config": "04a_LM_with_normalized_embeddings_mixer_softmax.ipynb",
          "optimize_permutation": "01_train_LM_en.ipynb",
          "optimize_model": "01_train_LM_en.ipynb",
          "aza_loss": "01_train_LM_en.ipynb",
@@ -17,11 +17,13 @@ index = {"split_file_into_chunks": "01_train_LM_en.ipynb",
          "top_n_translation_acc": "03_translate_en_to_es.ipynb",
          "PermuteEmbeddingNorm": "04_LM_with_normalized_embeddings.ipynb",
          "p_normAWD_LSTM": "04_LM_with_normalized_embeddings.ipynb",
+         "PermuteEmbeddingMixerNorm": "04a_LM_with_normalized_embeddings_mixer_softmax.ipynb",
+         "p_mixer_normAWD_LSTM": "04a_LM_with_normalized_embeddings_mixer_softmax.ipynb",
          "embs_to_txt": "05_aligning_the_embeddings_using_vecmap.ipynb",
          "txt_to_embs": "05_aligning_the_embeddings_using_vecmap.ipynb"}
 
 modules = ["core.py",
-           "decoder_head/data.py",
+           "data.py",
            "decoder_head/core.py"]
 
 doc_url = "https://earthspecies.github.io/decoder_head/"
